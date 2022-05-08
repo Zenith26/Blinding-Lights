82cb37feeba00f33523e9731e36189d0e3336f9d
reevera <104969280+reevera@users.noreply.github.com>
2022-05-07 22:34:59 -0400

    Revert "Bullet with bullet position"
    This reverts commit e2d916b627222c8e85a02015bc05ade32e7377f4.

--- BlindingLights/Assets/Script/GameMode/GameplayStatics.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// create a static class that will be accessed to everything
-public static class GameplayStatics
-{
-    
-}
