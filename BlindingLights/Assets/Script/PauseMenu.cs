dbff1d2cbc026b239be2764ae66d561dcf834854
RatKev23 <93149796+RatKev23@users.noreply.github.com>
2022-05-13 12:43:43 -0400

    Revert "New Work"
    This reverts commit f5e00bf084018ece37e656005779e9b25d3b6a21.

--- BlindingLights/Assets/Script/PauseMenu.cs
+++ BlindingLights/Assets/Script/PauseMenu.cs
@@ -1,50 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameisPaused;
-    public GameObject PauseMenuUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            if(GameisPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
-
-    public void Resume()
-    {
-        PauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameisPaused = false;
-    }
-
-    void Pause()
-    {
-        PauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameisPaused = true;
-    }
-
-    public void LoadMenu()
-    {
-        Time.timeScale = 1f; 
-        SceneManager.LoadScene("Level 1");
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
+        
     }
 }
