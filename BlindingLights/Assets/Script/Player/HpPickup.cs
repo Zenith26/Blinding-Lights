6d2522242e612ffe18a34478b91365cd94228b19
reevera <104969280+reevera@users.noreply.github.com>
2022-05-21 20:58:06 -0400

    Player HP Pickup

---
+++ BlindingLights/Assets/Script/Player/HpPickup.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpPickup : MonoBehaviour
+{
+    PlayerHealth phealth;
+    public float healthbonus = 1;
+
+
+    private void Start()
+    {
+        phealth = GameObject.FindObjectOfType<PlayerHealth>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(phealth.health < 3)
+        {
+            Destroy(gameObject);
+            phealth.health = phealth.health + healthbonus;
+        }
+    }
+}
